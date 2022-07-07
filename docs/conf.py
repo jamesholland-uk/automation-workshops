246c934bcd537b6f82d17b381fbd1ad56602c474
James <jamoiholland@gmail.com>
2022-07-07 10:54:20 +0100

    Happy new year!

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Automation Workshops'
-copyright = '2021, James Holland'
-author = 'James Holland'
+project = "Automation Workshops"
+copyright = "2021-2022, James Holland"
+author = "James Holland"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,20 +27,18 @@ author = 'James Holland'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx_copybutton'
-]
+extensions = ["sphinx_copybutton"]
 copybutton_prompt_text = "$ "
 copybutton_here_doc_delimiter = "EOL"
 copybutton_selector = "div.copy-button pre"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,15 +46,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # Pointing to custom CSS to customise the look and feel
-#html_css_files = ['custom.css']
+# html_css_files = ['custom.css']
 def setup(app):
-    app.add_css_file('custom.css')
+    app.add_css_file("custom.css")
